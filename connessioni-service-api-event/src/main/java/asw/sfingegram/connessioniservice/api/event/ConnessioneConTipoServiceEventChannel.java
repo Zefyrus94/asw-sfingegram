0a5093b84d047301e115f985ee8e24df0b2ece1e
danber93 <daniele.bertillo@gmail.com>
2021-05-21 17:52:59 +0200

    added connessioni publishers

---
+++ connessioni-service-api-event/src/main/java/asw/sfingegram/connessioniservice/api/event/ConnessioneConTipoServiceEventChannel.java
@@ -0,0 +1,5 @@
+package asw.sfingegram.connessioniservice.api.event;
+
+public class ConnessioneConTipoServiceEventChannel(){
+public static final String channel = "connessione-con-tipo-event-channel";
+        }
---
+++ connessioni/src/main/java/asw/sfingegram/connessioni/eventpublisher/ConnessioniConAutoreDomainEventPublisherImpl.java
@@ -0,0 +1,31 @@
+package asw.sfingegram.connessioni.eventpublisher;
+
+import asw.sfingegram.common.api.event.DomainEvent;
+import asw.sfingegram.connessioniservice.api.event.ConnessioneConAutoreServiceChannel;
+import asw.sfingegram.connessioni.domain.ConnessioneConAutoreDomainEventPublisher;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.kafka.core.KafkaTemplate;
+
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.logging.Logger;
+
+@Component
+public class ConnessioniConAutoreDomainEventPublisherImpl implements ConnessioneConAutoreDomainEventPublisher {
+
+    private final Logger logger = Logger.getLogger(ConnessioniDomainEventPublisherImpl.class.toString());
+
+    @Autowired
+    private KafkaTemplate<String, DomainEvent> template;
+
+    private String channel = ConnessioneConAutoreServiceChannel.channel;
+
+    @Override
+    public void publish(DomainEvent event) {
+        logger.info("PUBLISHING EVENT: " + event.toString() + " ON CHANNEL: " + channel);
+        template.send(channel, event);
+        // template.flush();
+    }
+}
