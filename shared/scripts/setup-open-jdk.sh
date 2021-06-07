9f9ce14a4c8591ace3e506a793b915d6adb1a6cb
Zefyrus94 <giacomo.bonanni94@gmail.com>
2021-06-07 11:00:47 +0200

    cluster kubernetes per asw-sfingegram (vedi kube-cluster)

---
+++ shared/scripts/setup-open-jdk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source "/home/asw-sfingegram/shared/scripts/common.sh"
+
+# see https://openjdk.java.net/install/ 
+
+# set up Java constants 
+# OPENJDK_PACKAGE=openjdk-8-jdk
+OPENJDK_PACKAGE=openjdk-14-jdk
+
+echo "==================="
+echo "installing open jdk"
+echo "==================="
+
+apt-get update 
+apt-get install -y ${OPENJDK_PACKAGE}
