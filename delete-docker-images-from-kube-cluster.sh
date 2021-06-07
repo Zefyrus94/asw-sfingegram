be077ac1d9c27c234988ea24195cb07b47e19ee4
Zefyrus94 <giacomo.bonanni94@gmail.com>
2021-06-07 21:10:23 +0200

    i contenitori vengono avviati e viene esposto l'ingress, ma non è ancora possibile contattare il servizio enigmi curl hello:31080 per una lista dei vecchi endpoint

--- delete-docker-images-from-kube-cluster.sh
+++ delete-docker-images-from-kube-cluster.sh
@@ -4,9 +4,10 @@ docker -H kube-2 image rm  sfingegram/enigmi-service
 docker -H kube-2 image rm  sfingegram/connessioni-service
 docker -H kube-2 image rm  sfingegram/enigmi-seguiti-service
 docker -H kube-2 image rm  sfingegram/api-gateway-service
+#docker -H kube-2 image rm  postgres
 
 docker -H kube-3 image rm  sfingegram/enigmi-service
 docker -H kube-3 image rm  sfingegram/connessioni-service
 docker -H kube-3 image rm  sfingegram/enigmi-seguiti-service
 docker -H kube-3 image rm  sfingegram/api-gateway-service
-
+#docker -H kube-3 image rm  postgres
