43a2983533adc67bbdbce2b610bd23202de11415
c1affa <michele.ciaffarafa@gmail.com>
2021-06-13 13:21:00 +0200

    Aggiunto connessioni allo sfingegram-application.yaml. Modificato lo script di delete del cluster.

--- delete-docker-images-from-kube-cluster.sh
+++ delete-docker-images-from-kube-cluster.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-docker -H kube-2 image rm  sfingegram/enigmi-service
-docker -H kube-2 image rm  sfingegram/connessioni-service
-docker -H kube-2 image rm  sfingegram/enigmi-seguiti-service
-docker -H kube-2 image rm  sfingegram/api-gateway-service
+#docker -H kube-2 image rm  sfingegram/enigmi-service
+docker -H kubegram-2 image rm  sfingegram/connessioni-service
+#docker -H kube-2 image rm  sfingegram/enigmi-seguiti-service
+#docker -H kube-2 image rm  sfingegram/api-gateway-service
 #docker -H kube-2 image rm  postgres
 
-docker -H kube-3 image rm  sfingegram/enigmi-service
-docker -H kube-3 image rm  sfingegram/connessioni-service
-docker -H kube-3 image rm  sfingegram/enigmi-seguiti-service
-docker -H kube-3 image rm  sfingegram/api-gateway-service
+#docker -H kube-3 image rm  sfingegram/enigmi-service
+docker -H kubegram-3 image rm  sfingegram/connessioni-service
+#docker -H kube-3 image rm  sfingegram/enigmi-seguiti-service
+#docker -H kube-3 image rm  sfingegram/api-gateway-service
 #docker -H kube-3 image rm  postgres
